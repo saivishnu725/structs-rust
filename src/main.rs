335664c30d0363e50328f74cedd3b7f377d8d0db
Sai Vishnu <saivishnu725@gmail.com>
2021-09-27 22:55:08 +0530

    Created a User struct and a user1 variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+struct User {
+    username: String,
+    emial: String,
+    active: bool,
+    password: i8,
+}
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        username: String::from("saivishnu725"),
+        email: String::from("saivishnu725@gmail.com"),
+        active: true,
+        password: 1234,
+    };
 }
