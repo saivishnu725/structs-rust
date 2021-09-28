88344b333b2031969a13fd15b5e3618326799456
Sai Vishnu <saivishnu725@gmail.com>
2021-09-28 22:38:44 +0530

    ready

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,17 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn is_equal(&self, other: &Rectangle) -> bool {
+        self.width == other.width && self.height == other.height
+    }
+}
+
 fn main() {
     let user1 = User {
         username: String::from("saivishnu725"),
@@ -28,11 +39,23 @@ fn main() {
         height: 10,
     };
     println!("rect: {:#?}", rect);
-    let a = area(&rect);
+    let a = rect.area();
+    //let a = area(&rect);
     //let a = rect.width * rect.height;
     println!("Area = {}", a);
+
+    let rect2 = Rectangle {
+        width: 20,
+        height: 10,
+    };
+    let rect3 = Rectangle {
+        width: 30,
+        height: 20,
+    };
+    println!("rect == rect2 : {}", rect.is_equal(&rect2));
+    println!("rect == rect3 : {}", rect.is_equal(&rect3));
 }
 
-fn area(r: &Rectangle) -> u32 {
+/* fn area(r: &Rectangle) -> u32 {
     r.width * r.height
-}
+} */
